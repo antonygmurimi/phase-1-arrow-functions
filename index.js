845ab3f5f903e3e4382fe225621965ec6ff6d3cb
Antony <antonygmurimi@gmail.com>
2024-03-31 23:05:39 +0300

    arrow function to multiply,divide,square and add two parameters

---
+++ index.js
@@ -0,0 +1,20 @@
+const divide = (parameter1, parameter2) =>{
+    console.log('divide ${parameter1}');
+    console.log('divide ${parameter2}');
+    return parameter1 / parameter2;
+}
+
+    const square = (parameter1) =>{
+        console.log('square ${parameter1}');
+       
+        return parameter1 * parameter1;
+}
+const add = (parameter1, parameter2) =>{
+    console.log('add ${parameter1}');
+    console.log('add ${parameter2}');
+    return parameter1 + parameter2;
+}
+
+sum(2000,100);
+square(100);
+add(12,7)
